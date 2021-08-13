ab8f1d44a695ae2cd197bc347d44ba37f3a9cf70
Saul Montes De Oca <mr.sauce97@gmail.com>
2021-08-13 09:36:58 -0500

    🎸 Playlist components integration

---
+++ frontend/src/components/SongList/index.js
@@ -0,0 +1,173 @@
+import React, { useImperativeHandle } from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import Divider from '@material-ui/core/Divider';
+import Typography from '@material-ui/core/Typography';
+import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
+import ListItemText from '@material-ui/core/ListItemText';
+import ListItemAvatar from '@material-ui/core/ListItemAvatar';
+import Checkbox from '@material-ui/core/Checkbox';
+import Avatar from '@material-ui/core/Avatar';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    paddingLeft: '3%',
+    paddingRight: '3%',
+    backgroundColor: '#191414',
+    fontWeight: 'bold',
+    color: '#B3B8BD',
+    fontFamily: 'Helvetica',
+  },
+  list: {
+    justifyContent: 'start',
+    backgroundColor: theme.palette.background.paper,
+  },
+  title: {
+    flex: 5,
+  },
+  album: {
+    flex: 3,
+  },
+  dateAdded: {
+    flex: 3,
+  },
+  duration: {
+    flex: 1,
+  },
+  number: {
+    flex: 0.3,
+    marginLeft: '1rem',
+  },
+  primary: {
+    color: '#FFFFFF',
+  },
+  secondary: {
+    color: '#B3B8BD',
+  }
+}));
+
+export default function CheckboxListSecondary(props) {
+  const classes = useStyles();
+
+  const getDate = (date) => {
+    date = new Date(date);
+    const year = date.getFullYear();
+    const month = date.getMonth() + 1;
+    const day = date.getDate();
+    let newMonth = '';
+
+    switch (month) {
+        case 1:
+            newMonth = 'Jan';
+            break;
+        case 2:
+            newMonth = 'Feb';
+            break;
+        case 3:
+            newMonth = 'Mar';
+            break;
+        case 4:
+            newMonth = 'Apr';
+            break;
+        case 5:
+            newMonth = 'May';
+            break;
+        case 6:
+            newMonth = 'Jun';
+            break;
+        case 7:
+            newMonth = 'Jul';
+            break;
+        case 8:
+            newMonth = 'Aug';
+            break;
+        case 9:
+            newMonth = 'Sep';
+            break;
+        case 10:
+            newMonth = 'Oct';
+            break;
+        case 11:
+            newMonth = 'Nov';
+            break;
+        case 12:
+            newMonth = 'Dec';
+            break;
+        default:
+            break;
+    }
+
+    return `${newMonth} ${day}, ${year}`;
+  }
+
+  const getDuration = (milliseconds) => {
+    let minutes = Math.floor(milliseconds / 60000);
+    let seconds = ((milliseconds % 60000) / 1000).toFixed(0);
+    return minutes + ":" + (seconds < 10 ? '0' : '') + seconds;
+  }
+
+  return (
+    <List dense className={classes.root}>
+        <ListItem>
+            <ListItemText className={classes.number} primary='#' />
+            <ListItemText className={classes.title} primary='TITLE'/>
+            <ListItemAvatar>
+            </ListItemAvatar>
+            <ListItemText className={classes.album} primary='ALBUM' />
+            <ListItemText className={classes.dateAdded} primary='DATE ADDED' />
+            <ListItemText className={classes.duration} primary='DURATION' />
+        </ListItem>
+        <Divider component="li" />
+      {props.songs.map((song, value) => {
+          const date_added = getDate(song.added_at);
+          const duration = getDuration(song.track.duration_ms);
+        return (
+          <ListItem key={value} button href={song.track.external_urls.spotify} target="_blank">
+            <ListItemText className={classes.number} primary={value + 1}  />
+            <ListItemAvatar>
+              <Avatar
+                variant="square"
+                alt={`Avatar n°${value + 1}`}
+                src={song.track.album.images[song.track.album.images.length - 1].url}
+              />
+            </ListItemAvatar>
+            <ListItemText className={classes.title} primary={
+                <React.Fragment>
+                    <Typography
+                            component="span"
+                            variant="body2"
+                            className={classes.primary}
+                        >
+                        {song.track.name}
+                    </Typography>
+                </React.Fragment>
+            } secondary={
+                    <React.Fragment>
+                        {song.track.artists.map((artist, artistIndex) => {
+                            return (
+                                <React.Fragment key={artistIndex}>
+                                    <Typography
+                                            component="span"
+                                            variant="body2"
+                                            // color="textSecondary"
+                                            className={classes.secondary}
+                                        >
+                                        {artist.name}
+                                        {artistIndex < song.track.artists.length - 1 ? ', ' : ''}
+                                    </Typography>
+                                </React.Fragment>
+                            );
+                        })
+                        }
+                    </React.Fragment>
+                } />
+            <ListItemText className={classes.album} primary={song.track.album.name} />
+            <ListItemText className={classes.dateAdded} primary={date_added} />
+            <ListItemText className={classes.duration} primary={duration} />
+          </ListItem>
+        );
+      })}
+    </List>
+  );
+}
--- frontend/src/layouts/Capture/index.js
+++ frontend/src/layouts/Capture/index.js
@@ -2,23 +2,42 @@ import React, {useState} from 'react'
 import WebcamCapture from './webcam'
 import { Grid } from '@material-ui/core';
 import Button from '@material-ui/core/Button';
+import axios from 'axios';
+import SongList from '../../components/SongList'
+import PlaylistTop from '../../components/PlaylistTop'
 
 import './capture.scss'
 
 function Capture(){
 
     const [capturedImage, setCapturedImage] = useState(null)
+    const [playlistData, setPlaylistData] = useState({});
 
     const onRetakeClick = () => {
         setCapturedImage(null)
+        setPlaylistData({});
     }
+    const getData = async (endpoint, setFunction) => {
+        try {
+            const { data } = await axios.get(endpoint, {
+                headers: {
+                    Authorization: `Bearer ${localStorage.getItem('token')}`
+                },
+            });
+            setFunction(data);
+            console.log(data);
+          } catch (error) {
+            console.log(error);
+        }
+    };
 
     const onSubmitClick = () => {
         //Redirect to Loading screen 
+        getData('https://api.spotify.com/v1/playlists/1pW6gAv6FdeHajhcbbldo9', setPlaylistData);
     }
 
     return (
-        <Grid container>
+        <Grid container >
             {capturedImage &&
             <>
                 <Grid container direction="column">
@@ -32,6 +51,20 @@ function Capture(){
                 </Grid>
             </>
             }
+            {
+                <Grid container direction="column">
+                    <Grid item xs={12}>
+                    {playlistData.tracks && 
+                    <>
+                        <Grid container style={{ height: 150, background: 'linear-gradient(0deg, rgba(25,20,20,1) 12%, rgba(25,20,20,0.6937149859943977) 46%, rgba(25,20,20,0.4192051820728291) 71%, rgba(25,20,20,0.27914915966386555) 80%, rgba(255,255,255,0) 95%)' }}></Grid>
+                        <PlaylistTop playlist={playlistData}/>
+                        <SongList songs={playlistData.tracks.items.slice(0,20)}/>
+                    </>}
+                </Grid>
+                </Grid>
+            }
+
+            
             {!capturedImage &&
                 <WebcamCapture setCapturedImage={setCapturedImage}/>}
         </Grid>
