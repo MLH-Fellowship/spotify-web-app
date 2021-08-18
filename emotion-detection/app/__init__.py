8ed09aad75eec65818e4f317b0aff780131a6dec
ccejudo <cristopher.cm1999@gmail.com>
2021-08-17 19:10:56 -0500

    Add Cross Origin

--- emotion-detection/app/__init__.py
+++ emotion-detection/app/__init__.py
@@ -1,7 +1,9 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 from . emotionDetection import getEmotion 
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route("/")
 def home():
